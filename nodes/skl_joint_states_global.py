1e18344e52686a54f2ebe1d4177a1e4bd9209e36
Shafagh Keyvanian <shkey@seas.upenn.edu>
2024-04-03 05:01:07 -0400

    updated from https://github.com/shafaghkey/human_model.git

--- nodes/skl_joint_states_global.py
+++ nodes/skl_joint_states_global.py
@@ -7,7 +7,7 @@ import rospy
 import sensor_msgs.msg
 from geometry_msgs.msg import Pose, PoseArray, PoseStamped
 from math import pi
-from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_matrix
+from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_matrix, euler_from_matrix
 from visualization_msgs.msg import Marker
 #MARKER ARRAY
 from visualization_msgs.msg import MarkerArray
@@ -77,19 +77,21 @@ class SKL:
         '''
         MarkerArr = MarkerArray()
 
-        for idx, bone in enumerate(msg.poses):               
-            # print(idx, bone)
+        # Hip to Head
+        R_rel = np.identity(3)
+        for idx in [0, 1, 2, 22, 23]:
+            bone = msg.poses[idx]
             self.bone_pose[idx,0] = bone.position.x 
             self.bone_pose[idx,1] = bone.position.y
             self.bone_pose[idx,2] = bone.position.z
             self.bone_pose[idx,3] = bone.orientation.x
             self.bone_pose[idx,4] = bone.orientation.y
             self.bone_pose[idx,5] = bone.orientation.z
-            self.bone_pose[idx,6] = bone.orientation.w
-            marker_idx = make_text (str(idx), [bone.position.x, bone.position.y, bone.position.z], idx)
-            MarkerArr.markers.append(marker_idx)
-
-            (ang1, ang2, ang3) = euler_from_quaternion(self.bone_pose[idx,3:], axes='rxyz') # ???
+            self.bone_pose[idx,6] = bone.orientation.w            
+            R_global = quaternion_matrix([bone.orientation.x, bone.orientation.y, bone.orientation.z, bone.orientation.w])
+            R_rel = R_rel.T @ R_global[:3,:3]
+            (ang1, ang2, ang3) = euler_from_matrix(R_rel, axes='rxyz')
+            # (ang1, ang2, ang3) = euler_from_quaternion(self.bone_pose[idx,3:], axes='rxyz') # ???
             # rospy.loginfo_throttle(5, str(self.bone_pose[idx,0]) + " " + str(self.bone_pose[idx,1]) + " " + str(self.bone_pose[idx,2]) + " " + str(self.bone_pose[idx,3]) + " " + str(self.bone_pose[idx,4]) + " " + str(self.bone_pose[idx,5]) + " " + str(self.bone_pose[idx,6]) )
 
             self.bone_pos[idx,0] = bone.position.x
@@ -99,6 +101,63 @@ class SKL:
             self.bone_rot[idx,1] = ang1
             self.bone_rot[idx,2] = ang3
             # rospy.loginfo ("idx: %s      bone_rot:  %s", idx, [round(180/pi*element, 2) for element in self.bone_rot[idx,:]] )
+            marker_idx = make_text (str(idx), [bone.position.x, bone.position.y, bone.position.z], idx)
+            MarkerArr.markers.append(marker_idx)
+
+        # Hip to Right Arm
+        R_rel = np.identity(3)
+        for idx in [0, 1, 2, 3, 4, 5, 6]:
+            bone = msg.poses[idx]
+            self.bone_pose[idx,0] = bone.position.x 
+            self.bone_pose[idx,1] = bone.position.y
+            self.bone_pose[idx,2] = bone.position.z
+            self.bone_pose[idx,3] = bone.orientation.x
+            self.bone_pose[idx,4] = bone.orientation.y
+            self.bone_pose[idx,5] = bone.orientation.z
+            self.bone_pose[idx,6] = bone.orientation.w            
+            R_global = quaternion_matrix([bone.orientation.x, bone.orientation.y, bone.orientation.z, bone.orientation.w])
+            R_rel = R_rel.T @ R_global[:3,:3]
+            (ang1, ang2, ang3) = euler_from_matrix(R_rel, axes='rxyz')
+            # (ang1, ang2, ang3) = euler_from_quaternion(self.bone_pose[idx,3:], axes='rxyz') # ???
+            # rospy.loginfo_throttle(5, str(self.bone_pose[idx,0]) + " " + str(self.bone_pose[idx,1]) + " " + str(self.bone_pose[idx,2]) + " " + str(self.bone_pose[idx,3]) + " " + str(self.bone_pose[idx,4]) + " " + str(self.bone_pose[idx,5]) + " " + str(self.bone_pose[idx,6]) )
+
+            self.bone_pos[idx,0] = bone.position.x
+            self.bone_pos[idx,1] = bone.position.y
+            self.bone_pos[idx,2] = bone.position.z
+            self.bone_rot[idx,0] = -ang2
+            self.bone_rot[idx,1] = ang1
+            self.bone_rot[idx,2] = ang3
+            # rospy.loginfo ("idx: %s      bone_rot:  %s", idx, [round(180/pi*element, 2) for element in self.bone_rot[idx,:]] )
+            marker_idx = make_text (str(idx), [bone.position.x, bone.position.y, bone.position.z], idx)
+            MarkerArr.markers.append(marker_idx)
+
+        # Hip to Left Arm
+        R_rel = np.identity(3)
+        for idx in [0, 1, 2, 24, 25, 26, 27]:
+            bone = msg.poses[idx]
+            self.bone_pose[idx,0] = bone.position.x 
+            self.bone_pose[idx,1] = bone.position.y
+            self.bone_pose[idx,2] = bone.position.z
+            self.bone_pose[idx,3] = bone.orientation.x
+            self.bone_pose[idx,4] = bone.orientation.y
+            self.bone_pose[idx,5] = bone.orientation.z
+            self.bone_pose[idx,6] = bone.orientation.w            
+            R_global = quaternion_matrix([bone.orientation.x, bone.orientation.y, bone.orientation.z, bone.orientation.w])
+            R_rel = R_rel.T @ R_global[:3,:3]
+            (ang1, ang2, ang3) = euler_from_matrix(R_rel, axes='rxyz')
+            # (ang1, ang2, ang3) = euler_from_quaternion(self.bone_pose[idx,3:], axes='rxyz') # ???
+            # rospy.loginfo_throttle(5, str(self.bone_pose[idx,0]) + " " + str(self.bone_pose[idx,1]) + " " + str(self.bone_pose[idx,2]) + " " + str(self.bone_pose[idx,3]) + " " + str(self.bone_pose[idx,4]) + " " + str(self.bone_pose[idx,5]) + " " + str(self.bone_pose[idx,6]) )
+
+            self.bone_pos[idx,0] = bone.position.x
+            self.bone_pos[idx,1] = bone.position.y
+            self.bone_pos[idx,2] = bone.position.z
+            self.bone_rot[idx,0] = -ang2
+            self.bone_rot[idx,1] = ang1
+            self.bone_rot[idx,2] = ang3
+            # rospy.loginfo ("idx: %s      bone_rot:  %s", idx, [round(180/pi*element, 2) for element in self.bone_rot[idx,:]] )
+            marker_idx = make_text (str(idx), [bone.position.x, bone.position.y, bone.position.z], idx)
+            MarkerArr.markers.append(marker_idx)
+
         self.text_pub.publish(MarkerArr)
         
         # self.bone_pos[0,:] -= self.kuka_pos[0,:]
